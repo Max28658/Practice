a5c896321fb74180d62f732390d46d6763831ad1
s28658 <s28658@pjwstk.edu.pl>
2024-03-19 11:33:38 +0100

    Modification 03

--- Practice_02/Program.cs
+++ Practice_02/Program.cs
@@ -21,5 +21,6 @@ public class Practice_02
 
         Console.WriteLine("max in array " + Task03.findMax(arr));
         Console.WriteLine("max in array " + Task03.findMax(arr));
+        Console.WriteLine("max in array " + Task03.findMax(arr));
     }
 }
